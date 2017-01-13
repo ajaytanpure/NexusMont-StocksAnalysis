5688a665e68fcc9d4129f65ac3ea149949fedb0c
AjayTanpure <ajaytanpure92@gmail.com>
2017-01-13 14:15:41 -0800

    removed zip file option in setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,6 @@ from distutils.core import setup
 import py2exe
 
 setup(windows=[{'script':'Main.py','icon_resources':[(1,'stockmarket.ico')],"dest_base":"NexusMont"}],
-      options = {"py2exe":{"dll_excludes":["MSVCP90.dll"]}})
+      options = {"py2exe":{"dll_excludes":["MSVCP90.dll"]}},
+      zipfile = None
+      )
